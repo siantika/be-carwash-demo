209386c9c50f90d85248f86390a42322d178de88
sian <csiantika@gmail.com>
2026-05-10 21:03:04 +0800

    (delete): delete duplicate code

--- app/modules/identity/application/dto/login_dto.py
+++ app/modules/identity/application/dto/login_dto.py
@@ -2,13 +2,6 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class LoginResultDto:
-    access_token: str
-    refresh_token: str
-    token_type: str
-
-
-@dataclass(frozen=True)
 class TokenPairDto:
     access_token: str
     refresh_token: str
