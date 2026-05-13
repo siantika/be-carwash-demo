168d2477d4de4d9ac15d576fac10df3cd45a2fff
sian <csiantika@gmail.com>
2026-05-13 15:23:44 +0800

    Inherits base attribute

--- app/modules/carwash_operation/domain/entities/ticket_void.py
+++ app/modules/carwash_operation/domain/entities/ticket_void.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 
-from app.shared.domain.entities.base import _utcnow
+from app.shared.domain.entities.base import BaseEntity, _utcnow
 from app.shared.domain.exceptions.exceptions import InvalidValueObject
 
 
 @dataclass(kw_only=True)
-class TicketVoid:
+class TicketVoid(BaseEntity):
     ticket_id: int
     account_id: int
     reason: str
